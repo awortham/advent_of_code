b63a3b37b15c033e53a74d70c61b2a1c4e9ccbd6
Aaron Wortham <wortham.aaron@gmail.com>
2022-01-21 12:11:56 -0700

    Day 7 part 2 solved

--- lib/day7.rb
+++ lib/day7.rb
@@ -1,38 +1,91 @@
 class Day7
   attr_reader :path
-  attr_accessor :final_position, :fuel
+  attr_accessor :subs
 
   def initialize(path:)
     @path = path
+    @subs = Subs.new(initial_subs_positions)
   end
 
   def align
-    subs = initial_subs_positions
+    subs.align
+  end
 
-    max = subs.max
-    min = subs.min
-    fuel_possibilities = (min..max)
+  def align_exponential
+    subs.align_exponential
+  end
 
-    foo = fuel_possibilities.each_with_object({}) do |number, acc|
-      sum = subs.map do |sub|
-        (sub - number).abs
+  def final_position
+    subs.final_position
+  end
+
+  def fuel
+    subs.fuel
+  end
+
+  private
+
+  def initial_subs_positions
+    load_data.split(",").map(&:to_i)
+  end
+
+  def load_data
+    File.read(path)
+  end
+end
+
+class Subs
+  attr_accessor :final_position, :fuel
+  attr_reader :initial_positions
+
+  def initialize(initial_positions)
+    @initial_positions = initial_positions
+  end
+
+  def align
+    figure_fuel_costs do |sub, number|
+      (sub - number).abs
+    end
+  end
+
+  def align_exponential
+    figure_fuel_costs do |sub, number|
+      figure_fuel_usage(sub, number)
+    end
+  end
+
+  def max
+    @_max ||= initial_positions.max
+  end
+
+  def min
+    @_min ||= initial_positions.min
+  end
+
+  private
+
+  def figure_fuel_costs(&block)
+    costs = fuel_possibilities.each_with_object({}) do |number, acc|
+      sum = initial_positions.map do |sub|
+        yield sub, number
       end.sum
+
       acc[number] = sum
     end
 
-    winner = [foo.min_by { |key, value| value }].to_h
+    winner = [costs.min_by { |key, value| value }].to_h
 
     self.final_position = winner.keys.first
     self.fuel = winner.values.first
   end
 
-  private
+  def figure_fuel_usage(start_position, end_position)
+    number_of_slots = (start_position - end_position).abs
 
-  def load_data
-    File.read(path)
+    (1..number_of_slots).sum
   end
 
-  def initial_subs_positions
-    load_data.split(",").map(&:to_i)
+  def fuel_possibilities
+    @_fuel_possibilities ||= min..max
   end
 end
--- spec/day7_spec.rb
+++ spec/day7_spec.rb
@@ -2,19 +2,26 @@ require "spec_helper"
 require "day7"
 
 describe Day7 do
-  it do
+  it "part 1" do
     subs = crabs_positions
     subs.align
 
-    require 'pry'; binding.pry
     expect(subs.final_position).to eq 2
     expect(subs.fuel).to eq 37
+  end
+
+  it "part 2" do
+    subs = crabs_positions
+    subs.align_exponential
+
+    expect(subs.final_position).to eq 5
+    expect(subs.fuel).to eq 168
   end
 
   private
 
   def crabs_positions
-    Day7.new(path: "lib/crabs.txt")
-    # Day7.new(path: "spec/support/crabs.txt")
+    # Day7.new(path: "lib/crabs.txt")
+    Day7.new(path: "spec/support/crabs.txt")
   end
 end
